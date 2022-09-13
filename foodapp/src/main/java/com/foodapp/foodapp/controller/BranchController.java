5c336059dc5ff086c4d770bee78cbc37fcf4e7c4
U6071047 <milanshingala666@gmail.com>
2022-09-13 16:36:26 +0530

    application propeties created

--- foodapp/src/main/java/com/foodapp/foodapp/controller/BranchController.java
+++ foodapp/src/main/java/com/foodapp/foodapp/controller/BranchController.java
@@ -2,20 +2,3 @@ package com.foodapp.foodapp.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.foodapp.entity.Branch;
-import com.foodapp.service.BranchService;
-import com.foodapp.responseStructure.ResponseStructure;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
